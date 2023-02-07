4f8962238c4a6a9bb868b5382a4f498a139c6fb8
KABANYANA <joyeusekabanyana@gmail.com>
2023-02-07 19:05:35 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,53 @@
+fun main( ) {
+   var N1 = 45
+    var N2 = 67
+    var Sum = N1+N2
+
+    println("the sum of two numbers is:"+" "+Sum)
+    var n1 = 45
+    var n2 = 67
+    var Diff = n2 - n1
+
+    println("the difference of two numbers is:"+" "+Diff)
+
+    var Num1 = 45.000
+    var Num2 = 67
+    var Product = Num1*Num2
+
+    println("the product of two numbers is:"+" "+Product)
+
+    var num1 = 45
+    var num2 = 67.000
+    var div = num1/num2
+
+    println("the quotient of two numbers is:"+" "+div)
+    var mod = num1 % num2
+    println(mod)
+
+    var a = 10
+    var b = 8
+    a+=b
+    println("new value is :"+""+a)
+    a-=b
+    println("The sustraction value is :"+""+a)
+    a*=b
+    println("The product value is :"+""+a)
+
+ // increment and decrement
+
+ a++
+ print("new value"+""+a)
+ a--
+ print("new value"+""+a)
+
+ println(a >= b)
+
+ var x= "umwana"
+ var y= "mama"
+ println(y>x)
+
+
+
+
+
+}
